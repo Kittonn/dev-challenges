2971c67a5730a8c11b8fa6ef3d9845a1a385f5ea
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-05-28 14:35:43 +0700

    Update

--- checkout-page/src/lib/Component/Box-Left.svelte
+++ checkout-page/src/lib/Component/Box-Left.svelte
@@ -6,32 +6,53 @@
   <div class="contact">
     <h3>Contact Information</h3>
     <label for="">E-mail</label>
-    <input type="text" placeholder="Enter your email..." />
+    <div class="input-container">
+      <i class="fa fa-envelope"></i>
+      <input required type="text" placeholder="Enter your email..." />
+    </div>
     <label for="">Phone</label>
-    <input type="text" placeholder="Enter your phone..." />
+    <div class="input-container">
+      <i class="fas fa-phone-alt"></i>
+      <input required type="text" placeholder="Enter your phone..." />
+    </div>
   </div>
 
   <div class="address">
     <h3>Shipping Address</h3>
     <label for="">Full name</label>
-    <input type="text" value="Rodney Cotton" />
+    <div class="input-container">
+      <i class="fa fa-user"></i>
+      <input required type="text" value="Rodney Cotton" />
+    </div>
     <label for="">Address</label>
-    <input type="text" placeholder="Your address.." />
+    <div class="input-container">
+      <i class="fa fa-home"></i>
+      <input required type="text" placeholder="Your address.." />
+    </div>
     <label for="">City</label>
-    <input type="text" placeholder="Your city.." />
+    <div class="input-container">
+      <i class="fa fa-city"></i>
+      <input required type="text" placeholder="Your city.." />
+    </div>
     <div class="flex-input">
       <div class="country">
         <label for="">Country</label>
-        <select name="" id="">
-          <option value="" disabled selected hidden>Your country..</option>
-          <option value="">Thailand</option>
-          <option value="">Japan</option>
-          <option value="">England</option>
-        </select>
+        <div class="input-container">
+          <i id="icon-country" class="fa fa-globe"></i>
+          <select name="" id="">
+            <option value="" disabled selected hidden>Your country..</option>
+            <option value="">Thailand</option>
+            <option value="">Japan</option>
+            <option value="">England</option>
+          </select>
+        </div>
       </div>
       <div class="code">
         <label for="">Postal code</label>
-        <input type="text" placeholder="Your postal code.." />
+        <div class="input-container">
+          <i class="fas fa-mail-bulk input-icon"></i>
+          <input required type="text" placeholder="Your postal code.." />
+        </div>
       </div>
     </div>
     <input type="checkbox" name="" id="" />
