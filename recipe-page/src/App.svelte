a3f97dce9555941780245b4af86353270fd226c3
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-05-29 23:58:14 +0700

    Recipe Page

---
+++ recipe-page/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Header from "./lib/Components/Header.svelte";
+  import ContentLeft from "./lib/Components/ContentLeft.svelte";
+  import ContentRight from "./lib/Components/ContentRight.svelte";
+</script>
+
+<div class="container">
+  <Header />
+  <div class="content">
+    <ContentLeft />
+    <ContentRight />
+  </div>
+</div>
---
+++ recipe-page/src/lib/data/cracker.ts
@@ -0,0 +1,8 @@
+export const cracker = [
+    {id:1,text1:"1 and 1/2 cups (150g)",textBold:"graham cracker crumbs",text2:" (about 10 full sheet graham crackers)"},
+    {id:2,text1:"5 Tablespoons (70g) ",textBold:"unsalted butter",text2:", melted"},
+    {id:3,text1:"1/4 cup (50g) ",textBold:"granulated sugar",text2:""},
+]
+
+
+
